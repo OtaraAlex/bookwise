201bc3e322721685bbbd641332a9650944c32525
OtaraAlex <mbechealex61@gmail.com>
2025-01-14 20:00:05 +0300

    home data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <Button>Click me</Button>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
