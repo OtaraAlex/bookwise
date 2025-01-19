c26b975c1e8a8e89dee419f91501ba06e4a29450
OtaraAlex <mbechealex61@gmail.com>
2025-01-19 19:02:05 +0300

    send emails and write workflows

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,42 @@
 import { auth } from "@/auth";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import { after } from "next/server";
+import { eq } from "drizzle-orm";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import Header from "@/components/Header";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/sign-in");
 
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get user and see if lastActivityDate is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
         <Header session={session} />
+
         <div className="mt-20 pb-20">{children}</div>
-        Footer
       </div>
     </main>
   );
